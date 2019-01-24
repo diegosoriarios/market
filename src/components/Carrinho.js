226c3874df721a60b61d7c64430c1c1c1537c3b1
diego.rios <diego.rios@CTNAPPLE01YG8WN.local>
2019-01-24 19:25:20 -0200

    Carrinho

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { ListGroup, ListGroupItem, Grid, Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { itemString, quantidadeString, addToItems } from '../actions/handlers';
 
@@ -7,7 +7,20 @@ class Carrinho extends Component {
     renderListItems = () => {
         if(this.props.items){
             return this.props.items.items.map((values, index) => {
-                return <ListGroupItem key={index}>{values.item} - {values.qtd}</ListGroupItem>
+                return (
+                    <ListGroupItem key={index}>
+                        <Grid>
+                            <Row className="show-grid">
+                                <Col xs={6} md={5}>
+                                    {values.item}
+                                </Col>
+                                <Col xs={1} md={1}>
+                                    {values.qtd}
+                                </Col>
+                            </Row>
+                        </Grid>
+                    </ListGroupItem>
+                );
             })
         }else{
             return <div>...</div>
